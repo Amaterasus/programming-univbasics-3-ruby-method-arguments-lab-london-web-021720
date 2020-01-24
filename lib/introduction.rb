ef10770cae87eda5fa0e9b809caed80ed219a35f
Skye Geraghty <skyetay03@googlemail.com>
2020-01-24 03:56:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,6 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program with #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
